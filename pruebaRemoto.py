2daae8ccfd6261ac8cb09dabca8577d59053f3ba
ceballosavilescode <ceballosavilescode@gmail.com>
2025-05-13 01:32:45 -0400

    Update pruebaRemoto.py

--- pruebaRemoto.py
+++ pruebaRemoto.py
@@ -40,39 +40,19 @@ def descargar_disponibilidad_devengos(variables):
 
 	print(f"🌍 Navegando a: {url}")
 	driver.get(url)
-	time.sleep(20)
 	
 	# Intentar hacer clic en el botón de exportar con espera incremental
-	wait_time = 10
-	while True:
-		try:
-			wait = WebDriverWait(driver, wait_time)
-			menu_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="export"]/span/span[2]')))
-			menu_button.click()
-			break  # Salir del bucle si el clic es exitoso
-		except (TimeoutException, NoSuchElementException) as e:
-			print(f"⚠️ Fallo al encontrar botón de exportar para - con espera de {wait_time}s. Error: {e}")
-			wait_time += 10  # Aumentar espera en 10 segundos
-			print(f"🔄 Reintentando con espera de {wait_time}s...")
-			if wait_time > 20:  # Límite máximo de espera (por ejemplo, 20 segundos)
-				print(f"❌ Tiempo máximo alcanzado para -. Marcando como Error.")
-				raise TimeoutException("Tiempo máximo de espera alcanzado")
+	wait = WebDriverWait(driver, 10)
 
+	# Intentar hacer clic en el botón de exportar
+
+	menu_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="export"]/span/span[2]')))
+	menu_button.click()
 	time.sleep(2)
 
-	# Intentar hacer clic en la opción "Como Excel" con espera incremental
-	wait_time = 10
-	while True:
-		try:
-			wait = WebDriverWait(driver, wait_time)
-			opcion_excel = wait.until(EC.element_to_be_clickable((By.XPATH, '//p[normalize-space(text()) = "Como Excel"]')))
-			opcion_excel.click()
-			break  # Salir del bucle si el clic es exitoso
-		except (TimeoutException, NoSuchElementException) as e:
-			print(f"⚠️ Fallo al encontrar opción Excel para -- con espera de {wait_time}s. Error: {e}")
-			wait_time += 10  # Aumentar espera en 10 segundos
-			print(f"🔄 Reintentando con espera de --s...")
-			if wait_time > 20:  # Límite máximo de espera (por ejemplo, 20 segundos)
-				print(f"❌ Tiempo máximo alcanzado para --. Marcando como Error.")
-				raise TimeoutException("Tiempo máximo de espera alcanzado")
-
+	opcion_excel = wait.until(EC.element_to_be_clickable((
+		By.XPATH, '//p[normalize-space(text()) = "Como Excel"]'
+	)))
+	opcion_excel.click()
+	time.sleep(10)
+	print(f"✅ {cod_institucion} descargado correctamente.")
