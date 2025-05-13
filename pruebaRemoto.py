aefe2f5a1bb767e8054daf50d742e55baf176cda
ceballosavilescode <ceballosavilescode@gmail.com>
2025-05-13 02:21:41 -0400

    Update pruebaRemoto.py

--- pruebaRemoto.py
+++ pruebaRemoto.py
@@ -18,7 +18,7 @@ from selenium.common.exceptions import (
 	TimeoutException,
 	WebDriverException
 )
-
+#''''''''''''''''''''''''''''''''''''''
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
